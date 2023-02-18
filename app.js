2a1de73c9d5d1cae5a0a821fa891867aab2e142c
prudhivi <prudhivi.dev@gmail.com>
2023-02-18 08:01:04 +0000

    solution

---
+++ app.js
@@ -0,0 +1,133 @@
+const express = require("express");
+const { open } = require("sqlite");
+const sqlite3 = require("sqlite3");
+const path = require("path");
+
+const databasePath = path.join(__dirname, "moviesData.db");
+const app = express();
+app.use(express.json());
+
+let database = null;
+
+const initializeDBAndServer = async () => {
+  try {
+    database = await open({
+      filename: databasePath,
+      driver: sqlite3.Database,
+    });
+    app.listen(3000, () =>
+      console.log("Server Running at http://localhost:3000/ ")
+    );
+  } catch (error) {
+    console.log(`DB Error: ${error.message}`);
+    process.exit(1);
+  }
+};
+
+initializeDBAndServer();
+
+const convertMovieDBObjectToResponseObject = (dbObject) => {
+  return {
+    movieName: dbObject.movie_name,
+    movieId: dbObject.movie_id,
+    directorId: dbObject.director_id,
+    leadActor: dbObject.lead_actor,
+  };
+};
+
+//GET API MOVIE NAMES
+
+app.get("/movies/", async (request, response) => {
+  const getMovieNameQuery = `
+    SELECT * FROM movie`;
+
+  const moviesArray = await database.all(getMovieNameQuery);
+  response.send(
+    moviesArray.map((eachMovie) => ({ movieName: eachMovie.movie_name }))
+  );
+});
+
+//GET API MOVIE NAME based on movieId
+
+app.get("/movies/:movieId/", async (request, response) => {
+  const { movieId } = request.params;
+  const getMovieQuery = `
+    SELECT 
+      *
+    FROM 
+      movie 
+    WHERE 
+      movie_id = ${movieId};`;
+  const movie = await database.get(getMovieQuery);
+  response.send(convertMovieDBObjectToResponseObject(movie));
+});
+
+//Post Method API Create a Movie
+
+app.post("/movies/", async (request, response) => {
+  const { directorId, movieName, leadActor } = request.body;
+  const postMovieQuery = `
+  INSERT INTO
+    movie ( director_id, movie_name, lead_actor)
+  VALUES
+    (${directorId}, '${movieName}', '${leadActor}');`;
+  await database.run(postMovieQuery);
+  response.send("Movie Successfully Added");
+});
+
+//PUT Method Updates the details of a movie in the movie table based on the movie ID
+
+app.put("/movies/:movieId/", async (request, response) => {
+  const { directorId, movieName, leadActor } = request.body;
+  const { movieId } = request.params;
+  const updateMovieQuery = `UPDATE movie 
+    SET 
+    director_id = ${directorId},
+    movie_name =  '${movieName}',
+    lead_actor =    '${leadActor}'
+    WHERE movie_id = ${movieId};`;
+  await database.run(updateMovieQuery);
+  response.send("Movie Details Updated");
+});
+
+//DELETE METHOD API Deletes a movie from the movie table based on the movie ID
+
+app.delete("/movies/:movieId/", async (request, response) => {
+  const { movieId } = request.params;
+  const deleteMovieQuery = `DELETE FROM movie WHERE movie_id = ${movieId};`;
+  await database.run(deleteMovieQuery);
+  response.send("Movie Removed");
+});
+
+const convertDirectorDBObjectToResponseObject = (dbObject) => {
+  return {
+    directorId: dbObject.director_id,
+    directorName: dbObject.director_name,
+  };
+};
+
+//GET METHOD API Returns a list of all directors in the director table
+
+app.get("/directors/", async (request, response) => {
+  const getDirectorsQuery = `SELECT * FROM director`;
+  const directorsArray = await database.all(getDirectorsQuery);
+  response.send(
+    directorsArray.map((eachDirector) =>
+      convertDirectorDBObjectToResponseObject(eachDirector)
+    )
+  );
+});
+
+// GET METHOD API Returns a list of all movie names directed by a specific director
+app.get("/directors/:directorId/movies/", async (request, response) => {
+  const { directorId } = request.params;
+  const getDirectorMovieQuery = `SELECT movie_name FROM movie where director_id = ${directorId}`;
+  const directorMoviesArray = await database.all(getDirectorMovieQuery);
+  response.send(
+    directorMoviesArray.map((eachMovie) => ({
+      movieName: eachMovie.movie_name,
+    }))
+  );
+});
+
+module.exports = app;
